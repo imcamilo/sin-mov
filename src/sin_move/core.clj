f19d05d809fd593a59ef3b503224e7812d2d2094
Camilo Jorquera <camilo.jorquera.a@gmail.com>
2019-10-29 18:55:52 -0300

    feat: swagger and fix endpoint name

--- src/sin_move/core.clj
+++ src/sin_move/core.clj
@@ -5,7 +5,14 @@
 
 (def app
     (api
-    
+
+        {:swagger
+        {:ui "/"
+         :spec "/swagger.json"
+         :data {:info {:title "sin-mov"
+                       :description "Compojure usage for students and collaborators"}
+                :tags [{:name "api", :description "API dev with their tests"}]}}}
+
         (context "/api" []
         :tags ["api"]
     
@@ -15,7 +22,7 @@
             :summary "adds two numbers together"
             (ok {:result "Hello Clj!"}))
     
-        (GET "/merge" []
+        (GET "/sum" []
             :return {:result Long}
             :query-params [x :- Long, y :- Long]
             :summary "sum two values"
--- test/sin_move/core_test.clj
+++ test/sin_move/core_test.clj
@@ -9,14 +9,14 @@
 
 (deftest endpoints-test
 
-    (testing "Test GET request to / returns expected response"
+    (testing "Test GET request to /api returns expected response"
         (let [response (app (-> (mock/request :get  "/api")))
             body (parse-body (:body response))]
         (is (= (:status response) 200))
         (is (= (:result body) "Hello Clj!"))))
         
-    (tesing "Test GET request to /merge returns expected response"
-        (let [response (all (-> (mock/request :get "/api/merge?x=10&y=2")))
+    (testing "Test GET request to /sum returns expected response"
+        (let [response (app (-> (mock/request :get "/api/sum?x=10&y=2")))
             body (parse-body (:body response))]
         (is (= (:status response) 200))
         (is (= (:result body) 12)))))
