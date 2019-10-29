e145cb3d9129b63156da8d531130741ebcc2f3af
Camilo Jorquera <camilo.jorquera.a@gmail.com>
2019-10-28 22:21:39 -0300

    feat: firt endpoint with string body

--- src/sin_mov/core.clj
+++ src/sin_mov/core.clj
@@ -1,7 +1,32 @@
 (ns sin-mov.core
+  (:require [org.httpkit.server :as server]
+            [compojure.core :refer :all]
+            [compojure.route :as route]
+            [ring.middleware.defaults :refer :all]
+            [clojure.pprint :as pp]
+            [clojure.string :as str]
+            [clojure.data.json :as json])
   (:gen-class))
 
+; Simple Page
+(defn simple-body-page [req]
+  {:status  200
+   :headers {"Content-Type" "text/html"}
+   :body    "Hello World"})
+
+
+; Our main routes
+(defroutes app-routes
+  (GET "/" [] simple-body-page)
+  (route/not-found "Error, page not found!"))
+
+; Our main entry function
 (defn -main
-  "I don't do a whole lot ... yet."
+  "This is our main entry point"
   [& args]
-  (println "Hello, World!"))
+  (let [port (Integer/parseInt (or (System/getenv "PORT") "8766"))]
+    ; Run the server with Ring.defaults middleware
+    (server/run-server (wrap-defaults #'app-routes site-defaults) {:port port})
+    ; Run the server without ring defaults
+    ;(server/run-server #'app-routes {:port port})
+    (println (str "Running webserver at http:/127.0.0.1:" port "/"))))
