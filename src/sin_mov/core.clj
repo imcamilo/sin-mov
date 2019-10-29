bea5d3ff4f73e29d083c5edcaa6dd0c4a7e740a0
Camilo Jorquera <camilo.jorquera.a@gmail.com>
2019-10-28 22:04:16 -0300

    first commit

---
+++ src/sin_mov/core.clj
@@ -0,0 +1,7 @@
+(ns sin-mov.core
+  (:gen-class))
+
+(defn -main
+  "I don't do a whole lot ... yet."
+  [& args]
+  (println "Hello, World!"))
---
+++ test/sin_mov/core_test.clj
@@ -0,0 +1,7 @@
+(ns sin-mov.core-test
+  (:require [clojure.test :refer :all]
+            [sin-mov.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
